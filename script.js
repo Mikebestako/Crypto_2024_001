6b8f031b94de15f7f90ff557edd386611fcd26f6
Mikebestako <138659153+Mikebestako@users.noreply.github.com>
2023-12-28 18:25:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,379 @@
+// =================================================================================================================
+// FAUCETS
+// =================================================================================================================
+    let faucet1A = document.createElement('input'); // Button
+    let faucet1B = document.createElement('input'); // Text
+
+    let faucet2A = document.createElement('input'); // Button
+    let faucet2B = document.createElement('input'); // Text
+
+    let faucet3A = document.createElement('input'); // Button
+    let faucet3B = document.createElement('input'); // Text
+
+    let faucet4A = document.createElement('input'); // Button
+    let faucet4B = document.createElement('input'); // Text
+
+    let faucet5A = document.createElement('input'); // Button
+    let faucet5B = document.createElement('input'); // Text
+
+    let faucet6A = document.createElement('input'); // Button
+    let faucet6B = document.createElement('input'); // Text
+
+    let faucet7A = document.createElement('input'); // Button
+    let faucet7B = document.createElement('input'); // Text
+
+    let faucet8A = document.createElement('input'); // Button
+    let faucet8B = document.createElement('input'); // Text
+
+    let faucet9A = document.createElement('input'); // Button
+    let faucet9B = document.createElement('input'); // Text
+
+    let faucet10A = document.createElement('input'); // Button
+    let faucet10B = document.createElement('input'); // Text
+
+    let faucet11A = document.createElement('input'); // Button
+    let faucet11B = document.createElement('input'); // Text
+
+    let faucet12A = document.createElement('input'); // Button
+    let faucet12B = document.createElement('input'); // Text
+
+    let faucet13A = document.createElement('input'); // Button
+    let faucet13B = document.createElement('input'); // Text
+
+    let faucet14A = document.createElement('input'); // Button
+    let faucet14B = document.createElement('input'); // Text
+
+    let faucet15A = document.createElement('input'); // Button
+    let faucet15B = document.createElement('input'); // Text
+
+    let faucet16A = document.createElement('input'); // Button
+    let faucet16B = document.createElement('input'); // Text
+
+// =================================================================================================================
+// CLAIMBITS
+// =================================================================================================================
+    let claim1A = document.createElement('input'); // Button
+    let claim1B = document.createElement('input'); // Text
+
+    let claim2A = document.createElement('input'); // Button
+    let claim2B = document.createElement('input'); // Text
+
+    let claim3A = document.createElement('input'); // Button
+    let claim3B = document.createElement('input'); // Text
+
+    let claim4A = document.createElement('input'); // Button
+    let claim4B = document.createElement('input'); // Text
+
+    let claim5A = document.createElement('input'); // Button
+    let claim5B = document.createElement('input'); // Text
+
+    let claim6A = document.createElement('input'); // Button
+    let claim6B = document.createElement('input'); // Text
+
+// =================================================================================================================
+// BAN FAUCET
+// =================================================================================================================
+    let ban1A = document.createElement('input'); // Button
+    let ban1B = document.createElement('input'); // Text
+
+    let ban2A = document.createElement('input'); // Button
+    let ban2B = document.createElement('input'); // Text
+
+    let ban3A = document.createElement('input'); // Button
+    let ban3B = document.createElement('input'); // Text
+
+    let ban4A = document.createElement('input'); // Button
+    let ban4B = document.createElement('input'); // Text
+
+    let ban5A = document.createElement('input'); // Button
+    let ban5B = document.createElement('input'); // Text
+
+    let ban6A = document.createElement('input'); // Button
+    let ban6B = document.createElement('input'); // Text
+
+// ===========================================================================================================================
+// FAUCETS
+// ===========================================================================================================================
+    createEntry('1XBITCOINS', 'a001', 'bp001', faucet1A, faucet1B, 'yellow', 'A01', '01:00', 'faucetCountdown1');
+    createEntry('EARNBITMOON', 'a002', 'bp002', faucet2A, faucet2B, 'aqua', 'A02', '05:00', 'faucetCountdown2');
+    createEntry('BANFAUCET', 'a003', 'bp003', faucet3A, faucet3B, 'pink', 'A03', '05:00', 'faucetCountdown3');
+    createEntry('VIEFAUCET', 'a004', 'bp004', faucet4A, faucet4B, 'greenyellow', 'A04', '05:00', 'faucetCountdown4');
+    createEntry('BITHUB', 'a005', 'bp005', faucet5A, faucet5B, 'orange', 'A05', '05:00', 'faucetCountdown5');
+    createEntry('COINFOLA', 'a006', 'bp006', faucet6A, faucet6B, 'darksalmon', 'A06', '10:00', 'faucetCountdown6');
+    createEntry('CRYPTOFLARE', 'a007', 'bp007', faucet7A, faucet7B, 'blanchedalmond', 'A07', '15:00', 'faucetCountdown7');
+    createEntry('LARVEL FAUCET', 'a008', 'bp008', faucet8A, faucet8B, 'cadetblue', 'A08', '30:00', 'faucetCountdown8');
+    createEntry('FAUCETCRYPTO', 'a009', 'bp009', faucet9A, faucet9B, 'yellow', 'A09', '40:00', 'faucetCountdown9');
+    createEntry('DUTCHY CORP 1', 'a010', 'bp010', faucet10A, faucet10B, 'aqua', 'A10', '30:00', 'faucetCountdown10');
+    createEntry('DUTCHY CORP 2', 'a011', 'bp011', faucet11A, faucet11B, 'pink', 'A11', '30:00', 'faucetCountdown11');
+    createEntry('COINTIPLY', 'a012', 'bp012', faucet12A, faucet12B, 'greenyellow', 'A12', '60:00', 'faucetCountdown12');
+    createEntry('FREE BITCOIN', 'a013', 'bp013', faucet13A, faucet13B, 'orange', 'A13', '60:00', 'faucetCountdown13');
+    createEntry('GRAB TC', 'a014', 'bp014', faucet14A, faucet14B, 'darksalmon', 'A14', '60:00', 'faucetCountdown14');
+    createEntry('LARVEL GAME', 'a015', 'bp015', faucet15A, faucet15B, 'yellow', 'A15', '05:00:00', 'faucetCountdown15');
+    createEntry('BITCOIN TRICKS', 'a016', 'bp016', faucet16A, faucet16B, 'aqua', 'A16', '10:00', 'faucetCountdown16');
+
+// ===========================================================================================================================
+// CLAIMBITS
+// ===========================================================================================================================
+    createEntry('CLAIMBITS FAUCET', 'f001', 'sp001', claim1A, claim1B, 'yellow', 'B01', '05:00', 'claimCountdown1');
+    createEntry('BITCO FAUCET 10', 'f002', 'sp002', claim2A, claim2B, 'yellow', 'B02', '10:00', 'claimCountdown2');
+    createEntry('BITCO FAUCET 30', 'f003', 'sp003', claim3A, claim3B, 'yellow', 'B03', '30:00', 'claimCountdown3');
+    createEntry('BITCO FAUCET 60', 'f004', 'sp004', claim4A, claim4B, 'yellow', 'B04', '60:00', 'claimCountdown4');
+    createEntry('BITCO FAUCET 90', 'f005', 'sp005', claim5A, claim5B, 'yellow', 'B05', '90:00', 'claimCountdown5');
+    createEntry('BITCO OFFERWALL', 'f006', 'sp006', claim6A, claim6B, 'yellow', 'B06', '27:00:00:00', 'claimCountdown6');
+
+// ===========================================================================================================================
+// BAN FAUCET
+// ===========================================================================================================================
+    createEntry('BAN FAUCET', 'g001', 'tp001', ban1A, ban1B, 'yellow', 'C01', '05:00', 'banCountdown1');
+    createEntry('BITCO FAUCET 10', 'g002', 'tp002', ban2A, ban2B, 'yellow', 'C02', '10:00', 'banCountdown2');
+    createEntry('BITCO FAUCET 30', 'g003', 'tp003', ban3A, ban3B, 'yellow', 'C03', '30:00', 'banCountdown3');
+    createEntry('BITCO FAUCET 60', 'g004', 'tp004', ban4A, ban4B, 'yellow', 'C04', '60:00', 'banCountdown4');
+    createEntry('BITCO FAUCET 90', 'g005', 'tp005', ban5A, ban5B, 'yellow', 'C05', '90:00', 'banCountdown5');
+    createEntry('BITCO OFFERWALL', 'g006', 'tp006', ban6A, ban6B, 'yellow', 'C06', '27:00:00:00', 'banCountdown6');
+
+    // Earn - 12/28/2023 - 489.311
+
+
+
+function countdown(element, countDownDate, button) {
+
+    // Fetch the display element
+    var el = document.getElementById(element);
+
+    // Set the timer
+    var interval = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+        var inMinutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24))
+        var hours = Math.floor((distance %(1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+        if(minutes > 0) {
+            var minute_text = minutes + (minutes > 1 ? ' minutes' : ' minute');
+        } else {
+            var minute_text = '';
+        }
+
+        if(hours > 0) {
+            var hour_text = hours + (hours > 1 ? ' hours' : ' hour');
+        } else {
+            var hour_text = ''
+        }
+
+        if(days > 0){
+            var day_text = days + (days > 1 ? ' days': ' day');
+        } else {
+            var day_text = '';
+        }
+
+        var second_text = seconds > 1 ? '' : '';
+        //el.value = minute_text + ' ' + seconds + ' ' + second_text + '';
+        //el.value = hour_text + ' ' + minute_text + ' ' + seconds + ' ' + second_text + 'seconds';
+        el.value = day_text + ' ' + hour_text + ' ' + minute_text + ' ' + seconds + ' ' + second_text + 'seconds';
+
+        if(minute_text == '' && hour_text == '' && day_text == ''){
+            if(seconds < 0){
+                seconds = 0;
+            }
+        }
+
+        if(seconds == 0) {
+            if(minute_text == '' && hour_text == '') {
+                (el.value = "STOP!");  
+                el.style.backgroundColor = 'pink';
+                clearInterval(interval);
+                button.disabled = false;     
+                return
+            }
+        } else {
+            el.style.backgroundColor = "blanchedalmond";
+        }
+
+    }, 1000);
+}
+
+
+function createEntry(value, id, id2, input1, input2, color, setTimer, timer, countDownTimer){
+    var div = document.getElementById(id);
+    var span1 = document.getElementById(id2);
+
+    span1.style.backgroundColor = color;
+    span1.textContent = value;
+
+    input1.type = "button";
+    input1.value = "SET TIMER " + setTimer;
+
+    input2.type = "text";
+    input2.value = timer;
+    input2.setAttribute("id", countDownTimer);
+
+    div.append(span1);
+    div.append(input1);
+    div.append(input2);
+
+    input1.style.width = '110px';
+    input2.style.width = '250px';
+}
+
+function setTimer(duration, setUrl, setPoints, setCountDown, input){
+    //countPoints += setPoints; 
+    //earnperHour += setPoints;
+    window.open(setUrl);
+    waitIn15Seconds(duration, setCountDown, input);    
+}
+
+// Wait 15 Seconds To Activate CountDown Timer
+function waitIn15Seconds(mins, strCounDown, button){  
+    var count = 0;
+    var tempInterval = setInterval(function(){
+    count++;
+    if(count == 10){
+        clearInterval(tempInterval);
+        startCountDown(mins, strCounDown, button)
+    }
+    }, 1000);
+}
+
+// To Set How many minutes to wait to claim BTC Satoshi
+function startCountDown(minutes, sCountDown, button){
+    let date = new Date();
+    var countDownDate = date.setMinutes(date.getMinutes() + minutes);
+    countdown(sCountDown, countDownDate, button);
+}
+
+let btn = document.querySelectorAll("input");
+
+for(let x in btn){
+    btn[x].onclick = function(){
+
+        btn[x].disabled = true;
+        switch(btn[x].defaultValue){
+        // =================================================================================================================
+        // FAUCETS
+        // =================================================================================================================
+            case 'SET TIMER A01':
+                setTimer(1, 'https://1xbitcoins.com/tifaucet', 1, 'faucetCountdown1', faucet1A);
+                break;
+
+            case 'SET TIMER A02':
+                setTimer(5, 'https://earnbitmoon.club/', 1.2, 'faucetCountdown2', faucet2A);
+                break;
+
+            case 'SET TIMER A03':
+                setTimer(5, 'https://banfaucet.com/faucet', 1, 'faucetCountdown3', faucet3A);
+                break;
+
+            case 'SET TIMER A04':
+                setTimer(5, 'https://viefaucet.com/app/faucet', 1.1, 'faucetCountdown4', faucet4A);
+                break;
+
+            case 'SET TIMER A05':
+                setTimer(5, 'https://bithub.win/faucet/key/wx1vldRgaI', 1, 'faucetCountdown5', faucet5A);
+                break;
+
+            case 'SET TIMER A06':
+                setTimer(10, 'https://coinfola.com/faucet#', 5, 'faucetCountdown6', faucet6A);
+                break;
+
+            case 'SET TIMER A07':
+                setTimer(15, 'https://cryptoflare.net/claim', 2, 'faucetCountdown7', faucet7A);
+                break;
+
+            case 'SET TIMER A08':
+                setTimer(30, 'https://larvelfaucet.com/faucet', 1, 'faucetCountdown8', faucet8A);
+                break;
+
+            case 'SET TIMER A09':
+                setTimer(40, 'https://faucetcrypto.com/dashboard', 1.74, 'faucetCountdown9', faucet9A);
+                break;
+
+            case 'SET TIMER A10':
+                setTimer(30, 'https://autofaucet.dutchycorp.space/roll.php', 1, 'faucetCountdown10', faucet10A);
+                break;
+
+            case 'SET TIMER A11':
+                setTimer(30, 'https://autofaucet.dutchycorp.space/coin_roll.php', 1, 'faucetCountdown11', faucet11A);
+                break;
+        
+            case 'SET TIMER A12':
+                setTimer(60, 'http://www.cointiply.com/home?intent=faucet', 5, 'faucetCountdown12', faucet12A);
+                break;
+
+            case 'SET TIMER A13':
+                setTimer(60, 'https://freebitco.in/', 2, 'faucetCountdown13', faucet13A);
+                break;
+
+            case 'SET TIMER A14':
+                setTimer(60, 'https://grab.tc/', 1.75, 'faucetCountdown14', faucet14A);
+                break;
+        
+            case 'SET TIMER A15':
+                setTimer(300, 'https://larvelfaucet.com/game', 3.75, 'faucetCountdown15', faucet15A);
+                break;
+
+            case 'SET TIMER A15':
+                setTimer(300, 'https://bitcointricks.com/?page=shortlinks', 3.75, 'faucetCountdown15', faucet16A);
+                break;
+
+
+                
+        
+        // =================================================================================================================
+        // CLAIMBITS
+        // =================================================================================================================
+            case 'SET TIMER B01':
+                setTimer(5, 'https://claimbits.net/faucet.html', 1, 'claimCountdown1', claim1A);
+                break;
+
+            case 'SET TIMER B02':
+                setTimer(10, 'https://claimbits.net/offers/bitcotasks.html', 1.2, 'claimCountdown2', claim2A);
+                break;
+
+            case 'SET TIMER B03':
+                setTimer(30, 'https://claimbits.net/offers/bitcotasks.html', 1, 'claimCountdown3', claim3A);
+                break;
+
+            case 'SET TIMER B04':
+                setTimer(60, 'https://claimbits.net/offers/bitcotasks.html', 1.1, 'claimCountdown4', claim4A);
+                break;
+
+            case 'SET TIMER B05':
+                setTimer(90, 'https://claimbits.net/offers/bitcotasks.html', 1, 'claimCountdown5', claim5A);
+                break;
+
+            case 'SET TIMER B06':
+                setTimer(38880, 'https://claimbits.net/offers/bitcotasks.html', 1, 'claimCountdown6', claim6A);
+                break;
+
+        // =================================================================================================================
+        // BAN FAUCET
+        // =================================================================================================================
+            case 'SET TIMER C01':
+                setTimer(5, 'https://banfaucet.com/faucet', 1, 'banCountdown1', ban1A);
+                break;
+
+            case 'SET TIMER C02':
+                setTimer(10, 'https://banfaucet.com/offerwall/bitcotasks', 1.2, 'banCountdown2', ban2A);
+                break;
+
+            case 'SET TIMER C03':
+                setTimer(30, 'https://banfaucet.com/offerwall/bitcotasks', 1, 'banCountdown3', ban3A);
+                break;
+
+            case 'SET TIMER C04':
+                setTimer(60, 'https://banfaucet.com/offerwall/bitcotasks', 1.1, 'banCountdown4', ban4A);
+                break;
+
+            case 'SET TIMER C05':
+                setTimer(90, 'https://banfaucet.com/offerwall/bitcotasks', 1, 'banCountdown5', ban5A);
+                break;
+
+            case 'SET TIMER C06':
+                setTimer(0, 'https://banfaucet.com/offerwall/bitcotasks', 1, 'banCountdown6', ban6A);
+                break;            
+        }
+    }
+}
